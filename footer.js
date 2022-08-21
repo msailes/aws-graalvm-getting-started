9d39a890058c632a57d7ee803ad4f27aa7433e25
msailes <msailes@users.noreply.github.com>
2022-08-21 20:49:39 +0000

    deploy: f83b33b62e97bbb69fee956916f3825a901eaf58

--- footer.js
+++ footer.js
@@ -3,8 +3,13 @@ footer = {
   "content" : [ {
     "type" : "Paragraph",
     "content" : [ {
-      "text" : "Contributions are welcome",
-      "type" : "SimpleText"
+      "url" : "https://github.com/msailes/aws-graalvm-getting-started",
+      "isFile" : false,
+      "type" : "Link",
+      "content" : [ {
+        "text" : "Contributions are welcome",
+        "type" : "SimpleText"
+      } ]
     } ]
   } ]
 }
--- toc.js
+++ toc.js
@@ -31,67 +31,23 @@ toc = [ {
     }, {
       "title" : "Advantages",
       "id" : "advantages"
-    } ]
-  } ]
-}, {
-  "chapterTitle" : "Chapter One",
-  "dirName" : "chapter-one",
-  "items" : [ {
-    "chapterTitle" : "Chapter One",
-    "pageTitle" : "Getting Started",
-    "pageMeta" : { },
-    "dirName" : "chapter-one",
-    "fileName" : "getting-started",
-    "viewOnRelativePath" : null,
-    "pageSectionIdTitles" : [ {
-      "title" : "Main Concepts",
-      "id" : "main-concepts"
     }, {
-      "title" : "Table of Contents",
-      "id" : "table-of-contents"
+      "title" : "Limitations",
+      "id" : "limitations"
     }, {
-      "title" : "Embedding Content",
-      "id" : "embedding-content"
-    }, {
-      "title" : "Meta",
-      "id" : "meta"
+      "title" : "Dependency Considerations",
+      "id" : "dependency-considerations"
     } ]
   }, {
-    "chapterTitle" : "Chapter One",
-    "pageTitle" : "Page Two",
+    "chapterTitle" : "Getting Started",
+    "pageTitle" : "Training Resources",
     "pageMeta" : { },
-    "dirName" : "chapter-one",
-    "fileName" : "page-two",
+    "dirName" : "getting-started",
+    "fileName" : "training-resources",
     "viewOnRelativePath" : null,
     "pageSectionIdTitles" : [ {
-      "title" : "Introduction",
-      "id" : "introduction"
-    } ]
-  } ]
-}, {
-  "chapterTitle" : "Chapter Two",
-  "dirName" : "chapter-two",
-  "items" : [ {
-    "chapterTitle" : "Chapter Two",
-    "pageTitle" : "Page Three",
-    "pageMeta" : { },
-    "dirName" : "chapter-two",
-    "fileName" : "page-three",
-    "viewOnRelativePath" : null,
-    "pageSectionIdTitles" : [ {
-      "title" : "Hello World",
-      "id" : "hello-world"
-    } ]
-  }, {
-    "chapterTitle" : "Chapter Two",
-    "pageTitle" : "Page Four",
-    "pageMeta" : { },
-    "dirName" : "chapter-two",
-    "fileName" : "page-four",
-    "viewOnRelativePath" : null,
-    "pageSectionIdTitles" : [ {
-      "title" : "Debugging",
-      "id" : "debugging"
+      "title" : "Additional Training Resources",
+      "id" : "additional-training-resources"
     } ]
   } ]
 } ]
